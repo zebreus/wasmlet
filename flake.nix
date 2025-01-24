b6bce3fb0917bbafc793e1a63a259fffa5a63a2e
Zebreus <zebreus@zebre.us>
2025-01-24 18:53:06 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "wasmer-interview-challenge";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            fenix.overlays.default
+          ];
+        };
+
+        fenixPkgs = fenix.packages.${system};
+        rustToolchain = fenixPkgs.combine [
+          fenixPkgs.complete.toolchain
+          fenixPkgs.targets.wasm32-unknown-unknown.stable.completeToolchain
+
+          (fenixPkgs.complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+        ];
+      in
+      rec {
+        name = "wasmer-interview-challenge";
+
+        devShell = pkgs.mkShell {
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+
+          buildInputs = [
+            rustToolchain
+            pkgs.rust-analyzer-nightly
+            pkgs.ldproxy
+            pkgs.wasm-tools
+            pkgs.cargo-rdme
+          ];
+        };
+
+        packages.default =
+          (pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          }).buildRustPackage
+            {
+              pname = "wasmer-interview-challenge";
+              version = "0.1.0";
+
+              src = ./wasmer-interview-challenge;
+
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+
+              nativeBuildInputs = [
+                pkgs.pkg-config
+              ];
+
+              meta = {
+                description = "wasmer toy project";
+                homepage = "https://github.com/zebreus/wasmer-interview-challenge";
+                license = pkgs.lib.licenses.agpl3Plus;
+              };
+            };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
