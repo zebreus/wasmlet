89abd6ecd653659d801c65460e56e0886ee73af3
Zebreus <zebreus@zebre.us>
2025-01-25 17:39:35 +0100

    Enable chaining multiple plugins

--- wasmlet/src/main.rs
+++ wasmlet/src/main.rs
@@ -5,7 +5,7 @@
 
 use clap::Parser;
 use plugin::Plugin;
-use std::path::PathBuf;
+use std::{path::PathBuf, process::ExitCode};
 mod plugin;
 
 /// Simple program to greet a person
@@ -20,22 +20,35 @@ struct Args {
     plugins: Vec<PathBuf>,
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
 
-    println!("Got {} plugins", args.plugins.len());
-    for plugin in args.plugins {
-        let mut plugin = match Plugin::new(plugin) {
-            Ok(plugin) => plugin,
-            Err(err) => {
-                eprintln!("Failed to load plugin: {}", err);
-                continue;
-            }
-        };
+    eprintln!("Got {} plugins", args.plugins.len());
+    let mut plugins = match args
+        .plugins
+        .into_iter()
+        .map(Plugin::new)
+        .collect::<Result<Vec<_>, _>>()
+    {
+        Ok(plugins) => plugins,
+        Err(err) => {
+            eprintln!("{}", err);
+            return 1.into();
+        }
+    };
 
-        let text = plugin.apply(&args.text).unwrap();
-        println!("{}", text);
-    }
+    let input_text: String = args.text.to_string();
+    let result = match plugins
+        .iter_mut()
+        .try_fold(input_text, |text, plugin| plugin.apply(&text))
+    {
+        Ok(result) => result,
+        Err(err) => {
+            eprintln!("{}", err);
+            return 1.into();
+        }
+    };
 
-    println!("{}", args.text);
+    println!("{}", result);
+    return 0.into();
 }
