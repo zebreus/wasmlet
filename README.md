d2c8d5168f89433f731bcc1a8b6784666abeb1be
Zebreus <zebreus@zebre.us>
2025-01-26 23:07:48 +0100

    Improve Readme

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ A simple program that formats text using WASM plugins.
 
 ## Usage
 
-Compile the plugin to wasm
+Compile the plugins to WASM
 
 ```sh
 cd rainbow
+cargo build --release
+cd ../bigfont
 cargo build --release
 ```
 
@@ -23,11 +25,20 @@ cargo build --release
 Use the plugin with WASMlet
 
 ```sh
-./target/release/wasmlet -p ../rainbow/target/wasm32-unknown-unknown/release/rainbow.wasm This is a rainbow
+./target/release/wasmlet -p bigfont -p rainbow WASMlet
 ```
 
 Expected output:
 
 ![expected-output](https://github.com/user-attachments/assets/28f5eea0-2c33-4d7d-bdfc-787c1d2513e1)
+
+## Plugin Resolution
+
+When you specify plugins with the `-p` flag, WASMlet uses the following strategy to find plugins:
+
+1. Try to interpret the specifier as a path to a file.
+2. Try the specifier with an appended `.wasm` extension.
+3. Try to load the specifier relative to the directory specified in `WASMLET_PLUGIN_DIR` (defaults to `/etc/wasmlet/plugins`).
+4. Try to load the specifier from a rust crate next to this project.
 
 <!-- cargo-rdme end -->
--- wasmlet/src/main.rs
+++ wasmlet/src/main.rs
@@ -4,10 +4,12 @@
 //!
 //! ## Usage
 //!
-//! Compile the plugin to wasm
+//! Compile the plugins to WASM
 //!
 //! ```sh
 //! cd rainbow
+//! cargo build --release
+//! cd ../bigfont
 //! cargo build --release
 //! ```
 //!
@@ -21,13 +23,21 @@
 //! Use the plugin with WASMlet
 //!
 //! ```sh
-//! ./target/release/wasmlet -p ../rainbow/target/wasm32-unknown-unknown/release/rainbow.wasm This is a rainbow
+//! ./target/release/wasmlet -p bigfont -p rainbow WASMlet
 //! ```
 //!
 //! Expected output:
 //!
 //! ![expected-output](https://github.com/user-attachments/assets/28f5eea0-2c33-4d7d-bdfc-787c1d2513e1)
 //!
+//! ## Plugin Resolution
+//!
+//! When you specify plugins with the `-p` flag, WASMlet uses the following strategy to find plugins:
+//!
+//! 1. Try to interpret the specifier as a path to a file.
+//! 2. Try the specifier with an appended `.wasm` extension.
+//! 3. Try to load the specifier relative to the directory specified in `WASMLET_PLUGIN_DIR` (defaults to `/etc/wasmlet/plugins`).
+//! 4. Try to load the specifier from a rust crate next to this project.
 #![feature(error_generic_member_access)]
 
 use clap::Parser;
